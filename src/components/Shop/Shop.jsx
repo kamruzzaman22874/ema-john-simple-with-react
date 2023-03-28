03677bf16f6771aadbca7176a26d451a7567da1e
kamruzzaman22874 <joybd.eee@gmail.com>
2023-03-28 12:40:30 +0600

    retrieve product from stored cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,8 +15,18 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getShoppingCart()
-        console.log(storedCart);
-    },[])
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+           
+        }
+        setCart(savedCart)
+    },[products])
 
 
      const handleAddToCart = (product) => {
