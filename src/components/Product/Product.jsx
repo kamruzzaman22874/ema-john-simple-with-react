2450cbea4fd3bd1504bd490701dc0dc3aafc3573
kamruzzaman22874 <joybd.eee@gmail.com>
2023-04-05 09:52:23 +0600

    first commit

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,8 +4,8 @@ import './Product.css'
   import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    const { img, name, price, ratings, seller, shipping, stock } = props.product
-   const handleAddToCart = props.handleAddToCart
+    const { img, name, price, ratings, seller} = props.product
+    const handleAddToCart = props.handleAddToCart
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -15,7 +15,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Ratting: {ratings}</p>
             </div>
-            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+            <button onClick={() => handleAddToCart(time)} className='btn-cart'>
                 Add to Cart
                 <FontAwesomeIcon icon={faShoppingCart} />
             </button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Home from './components/Home/Home'
+import Inventory from './components/Inventory/Inventory'
+import Login from './components/Login/Login'
+import Orders from './components/Orders/Orders'
+import Shop from './components/Shop/Shop'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: '/orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: '/inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
